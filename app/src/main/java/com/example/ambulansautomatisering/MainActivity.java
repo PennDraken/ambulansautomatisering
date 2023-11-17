c03189da08c886d2f3ca60a13b1f17cb3b43f8be
Henry Strömgren <89854592+PennDraken@users.noreply.github.com>
2023-11-17 11:45:13 +0100

    GPS location acquired (not used yet)

--- app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
+++ app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.ambulansautomatisering;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.Manifest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_LOCATION_PERMISSION = 1; // Add this line
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         // Check for permissions
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -30,9 +38,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getLocation() {
-        
+        // Create a location request
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Set up location callback
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    // Do something with the location, e.g., update UI
+                }
+            }
+        };
+
+        // Request location updates
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
     }
 
-
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, proceed with getting location
+                getLocation();
+            } else {
+                // Permission denied, handle accordingly (e.g., show a message or disable location features)
+            }
+        }
+    }
 }
