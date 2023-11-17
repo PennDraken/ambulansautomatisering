0fd9cca6451eb46e6264503104cca0aa9ec3fe70
Henry Strömgren <89854592+PennDraken@users.noreply.github.com>
2023-11-17 11:03:35 +0100

    GPS permission request

--- app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
+++ app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.ambulansautomatisering;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.Manifest;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1; // Add this line
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check for permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Request permissions
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        } else {
+            // Permission already granted, proceed with getting location
+            getLocation();
+        }
     }
+
+    private void getLocation() {
+        
+    }
+
+
+
 }
