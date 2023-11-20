45143e4ad1989e023c00d53457cdeae4cb9296dc
EricBlohm1 <89911464+EricBlohm1@users.noreply.github.com>
2023-11-20 13:14:14 +0100

    Working GPS, basic
    Code extracted from prompts in chatGPT. App works. Retrieves gps coordinates and shows them on the screen. Updates when the phone moves.

---
+++ app/src/main/java/com/example/ambulansautomatisering/LocationHelper.java
@@ -0,0 +1,62 @@
+package com.example.ambulansautomatisering;
+
+import android.content.Context;
+import android.location.Location;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+
+public class LocationHelper {
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private LocationListener locationListener;
+
+    public interface LocationListener {
+        void onLocationChanged(Location location);
+    }
+
+    public LocationHelper(Context context, LocationListener listener) {
+        fusedLocationProviderClient = new FusedLocationProviderClient(context);
+        locationListener = listener;
+        createLocationCallback();
+    }
+
+    private void createLocationCallback() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    if (locationListener != null) {
+                        locationListener.onLocationChanged(location);
+                    }
+                }
+            }
+        };
+    }
+
+    public void startLocationUpdates() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000); // Update interval in milliseconds
+        locationRequest.setFastestInterval(5000); // Fastest update interval
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        try {
+            fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    null /* Looper */
+            );
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+}
--- app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
+++ app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
@@ -4,11 +4,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.location.LocationServices;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity implements LocationHelper.LocationListener {
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private LocationHelper locationHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationHelper = new LocationHelper(this, this);
+        // Check for permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Request permissions
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        } else {
+            // Permission already granted, proceed with getting location
+
+            locationHelper.startLocationUpdates();
+        }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationHelper.startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationHelper.stopLocationUpdates();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Handle the updated location here
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        // Update the TextView with the new location
+        String locationText = "Latitude: " + latitude + "\nLongitude: " + longitude;
+        TextView locationTextView = findViewById(R.id.locationTextView);
+        locationTextView.setText(locationText);
+    }
+
 }
