a4b914a6236b12b796711daab8da89b3867fae74
EricBlohm1 <eric.blohm00@gmail.com>
2023-11-24 11:04:15 +0100

    Expanded location if checks. Also see if we have completed the previous time stamp, before proceeding.

--- app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
+++ app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
@@ -43,12 +43,22 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
     private static final int REQUEST_LOCATION_PERMISSION = 1;
     private LocationHelper locationHelper;
     private SensorManager sensorManager;
-    private Sensor accelerometerSensor;
 
-    private TextView accTextView;
+    private Date dt_overl;
 
-    public List<Task> tasks = new ArrayList<Task>();
+    private Date dt_adress;
 
+
+    /* create new task when pressing "kvittera" (button not implemented yet) add it to the list of active tasks.
+     This is because they may accept a new task before they left a patient at the hospital.
+     Should be a maximum of 2 tasks active. This level of abstraction may be redundant */
+    private List<Task> tasks = new ArrayList<Task>();
+
+    /* or something like the following to handle the case where they have 2 tasks.
+    public Task task1 = ...
+    public Task task2 = ...
+     */
+     private Task task1 = new Task(1);
 
     private final Coordinate ambulance_station = new Coordinate(57.7056, 11.8876); // Ruskvädersgatan 10, 418 34 Göteborg, Sweden
     private final Coordinate hospital_pos = new Coordinate(57.7219, 12.0498); // östra sjukhuset
@@ -91,6 +101,7 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
         locationHelper.startLocationUpdates();
     }
 
+    /* Remove? since we want to use the app in the back ground. */
     @Override
     // This is when activity loses focus
     protected void onPause() {
@@ -106,9 +117,8 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
 
         Coordinate current = new Coordinate(latitude, longitude);
 
-        Coordinate patient = new Coordinate(patient_position.getLatitude(), patient_position.getLongitude());
-
-        // Location outside 100m? then we've left hospital
+        /* Location outside 100m? then we've left ambulance station, this time stamp may be redundant.
+         change to "kvittering"? */
         if(isLocationOutsideThreshold(current, ambulance_station, 100) /* Check if this is the correct time stamp*/){
             // lägg till tidsnotering_n i lista?
 
@@ -123,13 +133,33 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
             locationTextView.setText(locationText);
         }
 
-        if(!isLocationOutsideThreshold(current, hospital_pos, 100) /*Also check if this is the correct time stamp*/ ){
-            // lägg till tidsnotering_n i lista?
-
+        /* Time stamp 2 */
+        if(!isLocationOutsideThreshold(current, patient_position, 100) && task1.getTS_checked(2)){
             // Get the current time
             long currentTimeMillis = System.currentTimeMillis();
             // Convert the time to a Date object if needed
             java.util.Date currentDate = new java.util.Date(currentTimeMillis);
+
+            // Save in external excel/txt?
+            dt_adress = currentDate;
+
+
+            // Update the TextView with the new location
+            String locationText = "Arrived at patient address";
+            TextView locationTextView = findViewById(R.id.locationTextView);
+            locationTextView.setText(locationText);
+        }
+
+        /* Time stamp 5 */
+        if(!isLocationOutsideThreshold(current, hospital_pos, 100) && task1.getTS_checked(5)){
+            // Get the current time
+            long currentTimeMillis = System.currentTimeMillis();
+            // Convert the time to a Date object if needed
+            java.util.Date currentDate = new java.util.Date(currentTimeMillis);
+
+            // Save in external excel/txt?
+            dt_overl = currentDate;
+
 
             // Update the TextView with the new location
             String locationText = "Arrived at hospital";
@@ -137,27 +167,6 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
             locationTextView.setText(locationText);
         }
 
-        if(!isLocationOutsideThreshold(current, patient_position, 100) /*Also check if this is the correct time stamp*/){
-            // lägg till tidsnotering_n i lista?
 
-            // Get the current time
-            long currentTimeMillis = System.currentTimeMillis();
-            // Convert the time to a Date object if needed
-            java.util.Date currentDate = new java.util.Date(currentTimeMillis);
-
-            // Update the TextView with the new location
-            String locationText = "Arrived at patient address";
-            TextView locationTextView = findViewById(R.id.locationTextView);
-            locationTextView.setText(locationText);
-        }
-        /*
-        else {
-            // Update the TextView with the new location
-            String locationText = "Idling";
-            TextView locationTextView = findViewById(R.id.locationTextView);
-            locationTextView.setText(locationText);
-        }
-
-         */
     }
 }
--- app/src/main/java/com/example/ambulansautomatisering/Task.java
+++ app/src/main/java/com/example/ambulansautomatisering/Task.java
@@ -6,11 +6,12 @@ import java.util.Date;
 public class Task {
     double tasknr;
     
-   /* Array with time stamps 1-6*/
+   /* Array with time stamps 1-6 */
     public Date[] time = new Date[6];
+
+
     /* Array with booleans for each time stamp, the boolean represent if we have passed the previous time stamp or not.
     E.g if we want to check for time stamp 4, then time stamp 1-3 have to be true */
-
    public Boolean[] time_checked = new Boolean[6];
 
     public Task(double tasknr) {
@@ -25,11 +26,11 @@ public class Task {
         return this.time[n-1] = time;
     }
 
-    public Boolean getTime_checked(Boolean[] time, int n) {
+    public Boolean getTS_checked(int n) {
         return time_checked[n-1];
     }
 
-    public Boolean setTime_checked(Boolean checked, int n) {
-        return this.time_checked[n-1] = checked;
+    public Boolean setTS_checked(Boolean b, int n) {
+        return this.time_checked[n-1] = b;
     }
 }
