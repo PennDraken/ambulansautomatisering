62bf5174fbf3b4e4d6b381a9278ea962c96bd2f2
Henry Strömgren <89854592+PennDraken@users.noreply.github.com>
2023-11-26 19:50:41 +0100

    implemented timestampmanager

--- app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
+++ app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
@@ -37,6 +37,7 @@ import android.content.pm.PackageManager;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -50,9 +51,8 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
     private Date dt_overl;
     private Date dt_adress;
 
-
     /* create new task when pressing "kvittera" (button not implemented yet). Should be a maximum of 2 tasks */
-     private Task task1 = new Task(1);
+    private Task task1 = new Task(1);
 
     private final Coordinate ambulance_station = new Coordinate(57.7056, 11.8876); // Ruskvädersgatan 10, 418 34 Göteborg, Sweden
     private final Coordinate hospital_pos = new Coordinate(57.7219, 12.0498); // östra sjukhuset
@@ -62,43 +62,36 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
     private final Coordinate patient_position = new Coordinate(6.8155, -5.2549);
 
 
-    private boolean isLocationOutsideThreshold(Coordinate current,
-                                               Coordinate target, float threshold) {
-        float[] results = new float[1];
-        Location.distanceBetween(current.getLatitude(), current.getLongitude(), target.getLatitude(), target.getLongitude(), results);
-        float distanceInMeters = results[0];
-        return distanceInMeters > threshold;
-    }
+
+    private TimeStampManager timeStampManager; // handles our timestamps
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // declares our timeStampManager
+        // we need to input our buttons we want to link as an array
+        // so that the manager updates the correct UI elements
+        Button[] buttons = new Button[6];
+        buttons[0] = findViewById(R.id.buttonTimeStamp1);
+        buttons[1] = findViewById(R.id.buttonTimeStamp2);
+        buttons[2] = findViewById(R.id.buttonTimeStamp3);
+        buttons[3] = findViewById(R.id.buttonTimeStamp4);
+        buttons[4] = findViewById(R.id.buttonTimeStamp5);
+        buttons[5] = findViewById(R.id.buttonTimeStamp6);
+        timeStampManager = new TimeStampManager(buttons);
         locationHelper = new LocationHelper(this, this);
 
-        // Makes our "tidsnotera" button clickable (links function onClick() to onCLick event)
+        // Makes our "tidsnotera" button clickable (links function onClick() below to onCLick event)
         Button buttonSetTime = findViewById(R.id.buttonSetTime);
         buttonSetTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Code to be executed when the button is clicked
-                // at the moment it just updates the text of the button
-
+                // Sets the time of the first non-empty timestamp
                 // Get current date, time and time zone.
                 java.util.Date currentDate = new java.util.Date();
-                // Format the date and time without including the time zone
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-
-                String current_time = sdf.format(currentDate);
-                buttonSetTime.setText(current_time);
-                // The code below is to reset the button text.
-                buttonSetTime.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        buttonSetTime.setText("Visa nuvarande tid och datum: ");
-                    }
-                }, 2000); // Adjust the delay as needed
+                timeStampManager.setTime(currentDate);
             }
         });
 
@@ -113,14 +106,14 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
         }
     }
 
-    // This function is called when the user presses the "Notera tid" button
-    public void noteTimeClick() {
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-        String currentTime = sdf.format(new Date());
-
-        // Display current time
-        Log.d("CurrentTime", "Current Time: " + currentTime);
+    private boolean isLocationOutsideThreshold(Coordinate current,
+                                               Coordinate target, float threshold) {
+        float[] results = new float[1];
+        Location.distanceBetween(current.getLatitude(), current.getLongitude(), target.getLatitude(), target.getLongitude(), results);
+        float distanceInMeters = results[0];
+        return distanceInMeters > threshold;
     }
+
 
     @Override
     // This is when activity is reopened
@@ -128,6 +121,8 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
         super.onResume();
         locationHelper.startLocationUpdates();
     }
+
+
 
     /* Remove? since we want to use the app in the back ground. */
     @Override
@@ -155,8 +150,7 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
         /* Location outside 100m? then we've left ambulance station, this time stamp may be redundant.
          change to "kvittering"? */
         if(isLocationOutsideThreshold(current, ambulance_station, 100) /* Check if this is the correct time stamp*/){
-            // lägg till tidsnotering_n i lista?
-
+            timeStampManager.setTime(0, currentDate);
             // Update the TextView with the new location
             String locationText = "Left station";
             TextView locationTextView = findViewById(R.id.locationTextView);
@@ -164,11 +158,8 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
         }
 
         /* Time stamp 2 */
-        if(!isLocationOutsideThreshold(current, patient_position, 100) && task1.getTS_checked(2)){
-            // Save in external excel/txt?
-            dt_adress = currentDate;
-
-
+        else if(!isLocationOutsideThreshold(current, patient_position, 100) && timeStampManager.isTimeStampChecked(1)){
+            timeStampManager.setTime(1, currentDate);
             // Update the TextView with the new location
             String locationText = "Arrived at patient address";
             TextView locationTextView = findViewById(R.id.locationTextView);
@@ -176,11 +167,9 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
         }
 
         /* Time stamp 5 */
-        if(!isLocationOutsideThreshold(current, hospital_pos, 100) && task1.getTS_checked(5)){
+        else if(!isLocationOutsideThreshold(current, hospital_pos, 100) && timeStampManager.isTimeStampChecked(4)){
             // Save in external excel/txt?
-            dt_overl = currentDate;
-
-
+            timeStampManager.setTime(4, currentDate);
             // Update the TextView with the new location
             String locationText = "Arrived at hospital";
             TextView locationTextView = findViewById(R.id.locationTextView);
--- app/src/main/java/com/example/ambulansautomatisering/TimeStampManager.java
+++ app/src/main/java/com/example/ambulansautomatisering/TimeStampManager.java
@@ -1,15 +1,68 @@
 package com.example.ambulansautomatisering;
+import android.widget.Button;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import androidx.appcompat.app.AppCompatActivity;
 
 // This class handles setting our timestamps
 // Used to abstract away the implementation of handling timestamps
 public class TimeStampManager {
     // Stores the dates and times of our different time stamps
-    public Date[] time = new Date[6];
+    private Date[] timeStamps = new Date[6];
+    private Button[] buttons;
 
 
-    // Constructor
-    public TimeStampManager() {
+    // Constructor (insert buttons which you want to set)
+    public TimeStampManager(Button[] buttons) {
+        this.buttons = buttons;
+    }
 
+    // Sets the most recent undefined timeStamp text to current time
+    public void setTime(Date timeToSet) {
+        for (int i=0;i<timeStamps.length;i++) {
+            Date date = timeStamps[i];
+            if (date == null) {
+                timeStamps[i] = timeToSet;
+                break; // Break because we set the time wanted to set
+            }
+        }
+        updateUI();
+    }
+
+    // Sets a specific timeStamp to a time
+    public void setTime(int timeStampIndex, Date timeToSet) {
+        timeStamps[timeStampIndex] = timeToSet;
+        updateUI();
+    }
+
+    // Updates the UI so text corresponds to our timeStamps
+    public void updateUI() {
+        for (int i=0;i<timeStamps.length;i++) {
+            Date date = timeStamps[i];
+            if (date!=null) {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+                String current_time = sdf.format(date);
+                buttons[i].setText(current_time);
+            } else {
+                buttons[i].setText("--:--");
+            }
+        }
+    }
+
+    // Checks if a time stamp has been filled in
+    public Boolean isTimeStampChecked(int index) {
+        return timeStamps[index] == null;
+    }
+
+    // Resets all timeStamps and updates UI
+    public void reset() {
+        this.timeStamps = new Date[6];
+    }
+
+    // Saves the dates to a file and resets the UI
+    public void save() {
+        // TODO not implemented yet
     }
 }
