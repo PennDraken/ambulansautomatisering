a50fb44927fae30de083df22836ac9a419076f6b
EricBlohm1 <eric.blohm00@gmail.com>
2023-11-29 14:03:02 +0100

    changed to accelerationsensor which excludes gravity

--- app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
+++ app/src/main/java/com/example/ambulansautomatisering/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
         //declare sensorManager
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         if (sensorManager != null) {
-            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         }
 
         accTextView = findViewById(R.id.accTextView);
@@ -221,7 +221,7 @@ public class MainActivity extends AppCompatActivity implements LocationHelper.Lo
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
             float x = event.values[0];
             float y = event.values[1];
             float z = event.values[2];
