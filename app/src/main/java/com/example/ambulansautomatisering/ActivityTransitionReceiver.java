19477e75bcee2e3d056513778456e72a8ef83621
Henry Strömgren <89854592+PennDraken@users.noreply.github.com>
2023-12-11 12:46:55 +0100

    Update ActivityTransitionReceiver.java

--- app/src/main/java/com/example/ambulansautomatisering/ActivityTransitionReceiver.java
+++ app/src/main/java/com/example/ambulansautomatisering/ActivityTransitionReceiver.java
@@ -29,9 +29,7 @@ public class ActivityTransitionReceiver extends BroadcastReceiver {
                 try {
                     MainActivity.getInstace().updateTheTextView(ActivityTransitionUtil.toActivityString(event.getActivityType()));
                     timeArray.add(new Tuple(System.currentTimeMillis() - timeStarted, ActivityTransitionUtil.toActivityString(event.getActivityType())));
-                } catch (Exception e) {
-
-                }
+                } catch (Exception e) {}
                 // Update estimated time of meeting patient
                 if (timeArray.size()!=0) {
                     // Get old date from timeStamp 1 (arrived at patient address) and add seconds to find estimated walk time
@@ -40,9 +38,8 @@ public class ActivityTransitionReceiver extends BroadcastReceiver {
                     currentDate.setTime(currentDate.getTime() + seconds);
                     MainActivity.timeStampManager.setTime(2, currentDate);
                 }
-
                 // Check if driving and has walked (we are probably driving away from the patient address)
-                if (ActivityTransitionUtil.toActivityString(event.getActivityType()) == "IN_VEHICLE") {
+                if (ActivityTransitionUtil.toActivityString(event.getActivityType()).equals("IN_VEHICLE")) {
                     // Update left patient address to current time
                     MainActivity.timeStampManager.setTime(3, new java.util.Date());
                 }
